e662a9e9c7f35b948a3e5d4f125ccff32355e9a1
kobudnik <105399943+kobudnik@users.noreply.github.com>
2023-03-07 16:04:34 -0500

    Template login and create user with fetch requests

--- client/src/routes/CreateAccount.jsx
+++ client/src/routes/CreateAccount.jsx
@@ -1,13 +1,10 @@
 import React from 'react';
 import { Outlet, Link, Form } from 'react-router-dom';
-import './index.css';
-import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 
-const App = () => {
-  const [loggedIn, setLoggedIn] = useState(false);
+const CreateAccount = () => {
   const navigate = useNavigate();
-  const sendLogin = async (e) => {
+  const createUser = async (e) => {
     e.preventDefault();
     const data = new FormData(e.target);
     console.log(data);
@@ -15,37 +12,38 @@ const App = () => {
     const password = data.get('password');
 
     console.log(username, password);
-    const loginBody = JSON.stringify({ username, password });
+    const userBody = JSON.stringify({ username, password });
 
-    const post = await fetch('/api/login', {
+    const post = await fetch(e.target.action, {
       method: 'POST',
-      body: loginBody,
+      body: userBody,
     });
 
-    if (post.status !== 200) navigate('/contacts/:contactId');
+    if (post.status !== 200) navigate('/');
   };
   return (
     <>
+      pro chacho
       <div id='login-body'>
         <div id='login-form-parent'>
-          <h1>Please Login</h1>
+          <h1>Welcome. Please create your account</h1>
           <div>
             <form
-              id='search-form'
+              action='/api/create'
+              id='new-user-form'
               role='form'
-              action='/api/login'
-              onSubmit={sendLogin}
+              onSubmit={createUser}
             >
               <input
                 className='login-submit'
-                aria-label='login-username-input'
+                aria-label='new-username-input'
                 placeholder='username'
                 type='text'
                 name='username'
               />
               <input
-                className='login-submit'
-                aria-label='login-password-input'
+                className='user-submit'
+                aria-label='new-password-input'
                 placeholder='password'
                 type='password'
                 name='password'
@@ -53,13 +51,11 @@ const App = () => {
               <button type='submit'>Submit</button>
             </form>
           </div>
-          <Link to={'/createaccount'} id='create-account-rdr'>
-            New here? Create an Account
-          </Link>
+          <Link to={'/login'} id='login-rdr'></Link>
         </div>
       </div>
     </>
   );
 };
 
-export default App;
+export default CreateAccount;
