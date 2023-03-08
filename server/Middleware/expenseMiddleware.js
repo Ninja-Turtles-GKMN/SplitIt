4741123f0f4406dd6327876d09100a824e1ea4df
GarrettYan <garrettyan6@gmail.com>
2023-03-07 19:05:36 -0500

    set up login logic between server and database

--- server/Middleware/expenseMiddleware.js
+++ server/Middleware/expenseMiddleware.js
@@ -4,13 +4,49 @@ module.exports = {
   addExpense: (req, res, next) => {
     const { event, date, amount, payer } = req.body;
     console.log(event);
+    const payerArray = [];
+    payer.forEach((name) => {
+      sql
+        .query(`SELECT user_id FROM users WHERE username = '${name}'`)
+        .then((data) => {
+          if (data.rows) {
+            payerArray.push(data.rows);
+          } else {
+            next('users not existed');
+          }
+        })
+        .catch((err) => console.log(err));
+    });
 
     sql
-      //   .query(`SELECT * FROM expenses`)
       .query(
-        `INSERT INTO expenses (description, date, total_amount, payer_id )
-         VALUES ('${event}', '${date}',${amount},${payer});`
+        `WITH data(event, date, amount, payer) AS (
+        VALUES                              
+           ('${event}', '${data}', ${amount}, '${payer}') 
+                       
+        )
+     , ins1 AS (
+        INSERT INTO expenses (event, date, amount)
+        SELECT event, data, amount        
+        FROM   data
+        RETURNING event, data, amount
+        )
+     , ins2 AS (
+        INSERT INTO debts (payer)
+        SELECT payer
+        FROM   data 
+        JOIN   ins1 USING (firstname, lastname)
+        RETURNING sample_id, user_id
+        )
+     `
       )
+
+      // sql
+      //   //   .query(`SELECT * FROM expenses`)
+      //   .query(
+      //     `INSERT INTO expenses (event, date, amount )
+      //      VALUES ('${event}', '${date}', ${amount});`
+      //   )
       .then((data) => {
         console.log('datarows', data.rows);
         next();
@@ -19,7 +55,9 @@ module.exports = {
   },
   payDebt: (req, res, next) => {
     const { payer, host, event } = req.body;
-    console.log(req.body);
+    sql.query(
+      `UPDATE debts SET is_paid = true WHERE debtor_id = '${host}' AND creditor_id = '${payer}'`
+    );
     next();
   },
 };
